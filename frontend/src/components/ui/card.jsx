59a3a95f60a8440bc5f6f1814cadb76178612799
Devadharshani B <devadharshai13@gmail.com>
2026-02-01 19:53:55 +0530

    Update card.jsx

--- frontend/src/components/ui/card.jsx
+++ frontend/src/components/ui/card.jsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils";
+
 
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
@@ -48,3 +49,4 @@ const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+
