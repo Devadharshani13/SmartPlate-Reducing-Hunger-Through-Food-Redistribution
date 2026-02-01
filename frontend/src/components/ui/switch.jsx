82cf9720721b67e1bc616e43a9d1fb82854cbe01
Devadharshani B <devadharshai13@gmail.com>
2026-02-01 20:01:08 +0530

    Update switch.jsx

--- frontend/src/components/ui/switch.jsx
+++ frontend/src/components/ui/switch.jsx
@@ -1,7 +1,8 @@
 import * as React from "react"
 import * as SwitchPrimitives from "@radix-ui/react-switch"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils";
+
 
 const Switch = React.forwardRef(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
@@ -20,3 +21,4 @@ const Switch = React.forwardRef(({ className, ...props }, ref) => (
 Switch.displayName = SwitchPrimitives.Root.displayName
 
 export { Switch }
+
